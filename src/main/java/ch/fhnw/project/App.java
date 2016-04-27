1543d398b57327b6fad436b6446587b127213838
saschadietrich <sascha.dietrich@students.fhnw.ch>
2016-04-27 19:16:01 +0200

    class for txt file 1 botton interface with load modul

--- src/main/java/ch/fhnw/project/App.java
+++ src/main/java/ch/fhnw/project/App.java
@@ -1,6 +1,10 @@
 package ch.fhnw.project;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 import java.util.function.Supplier;
@@ -9,9 +13,29 @@ public final class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        // Asserts correct project setup (will only compile with Java 8)
-        Supplier<String> helloWorldSupplier = () -> "Hello Stefan";
-        System.out.println(helloWorldSupplier.get());
+
+        Button bu1 = new Button("Test");
+        bu1.setOnAction(event -> loadfenster(stage));
+
+        Pane pane = new Pane();
+        pane.getChildren().add(bu1);
+
+
+        Scene scene = new Scene(pane);
+        stage.setScene(scene);
+        stage.setTitle("Testversion 0.0");
+        stage.show();
     }
 
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    private void loadfenster(Stage stage){
+        FileChooser chooser = new FileChooser();
+        chooser.setTitle("test");
+        String fileadress = chooser.showOpenDialog(stage).getAbsolutePath();
+        //TxtData one = new TxtData(fileadress,0);//geht aus ifgendeinem grund nicht
+
+    }
 }
