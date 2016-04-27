a55ad99d4db1e4b0b7eff746566ed299ffa140e1
saschadietrich <sascha.dietrich@students.fhnw.ch>
2016-04-27 12:53:41 +0200

    welcome message

--- src/main/java/ch/fhnw/project/App.java
+++ src/main/java/ch/fhnw/project/App.java
@@ -10,7 +10,7 @@ public final class App extends Application {
     @Override
     public void start(Stage stage) {
         // Asserts correct project setup (will only compile with Java 8)
-        Supplier<String> helloWorldSupplier = () -> "Hello Sascha";
+        Supplier<String> helloWorldSupplier = () -> "Hello Stefan";
         System.out.println(helloWorldSupplier.get());
     }
 
