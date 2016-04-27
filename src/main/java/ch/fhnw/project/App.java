478b609aab2f5d71dd5c6f5c4732a47930dfd1c3
stefankarlin <stefan.karlin@students.fhnw.ch>
2016-04-27 12:49:20 +0200

    Changed Hello to Sascha

--- src/main/java/ch/fhnw/project/App.java
+++ src/main/java/ch/fhnw/project/App.java
@@ -10,7 +10,7 @@ public final class App extends Application {
     @Override
     public void start(Stage stage) {
         // Asserts correct project setup (will only compile with Java 8)
-        Supplier<String> helloWorldSupplier = () -> "Hello World";
+        Supplier<String> helloWorldSupplier = () -> "Hello Sascha";
         System.out.println(helloWorldSupplier.get());
     }
 
