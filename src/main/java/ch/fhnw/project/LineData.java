a2e8caee38cb3fe9f8197bd031ccba91dc0712d8
stefankarlin <stefan.karlin@students.fhnw.ch>
2016-04-29 00:09:44 +0200

    Creating Class to read Line Datafiles -- Es laeuft :)

--- src/main/java/ch/fhnw/project/LineData.java
+++ src/main/java/ch/fhnw/project/LineData.java
@@ -6,14 +6,13 @@ import java.util.*;
 /**
  * Created by stefan on 28.04.16.
  */
+
 public class LineData {
 
     // Fields:
-    List<String> datalines;
-    List<String> variableNames = new ArrayList<>();
-    ArrayList<ArrayList> valueList = new ArrayList<>();
-    Map<String, ArrayList<Double>> dataset = new HashMap<>();
     String delimiter = "";
+    private List<String> variableNames = new ArrayList<>();
+    private Map<String, List<Double>> dataset = new HashMap<>();
 
     //Constructor:
 
@@ -22,9 +21,10 @@ public class LineData {
             File file = new File(filePath);
             Scanner fileScanner = new Scanner(file);
 
-            datalines = new ArrayList<>();
             int amountofVariables = Integer.parseInt(fileScanner.nextLine());
             int maxAmountofLines = 2 * (amountofVariables + 1);
+
+            List<List> valueList = new ArrayList<>();
 
             for (int row = amountofVariables; row <= maxAmountofLines; row++) {
                 if (row <= (amountofVariables + 1)) {
@@ -34,8 +34,11 @@ public class LineData {
                     fileScanner.useDelimiter(delimiter);
                 } else {
                     while (fileScanner.hasNextLine()) {
-                        String lines = fileScanner.nextLine();
-                        ArrayList<Double> arrayLine = new ArrayList(Arrays.asList(lines.split(delimiter)));
+                        List<String> lines = new ArrayList(Arrays.asList(fileScanner.nextLine().split(delimiter)));
+                        List<Double> arrayLine = new ArrayList<>();
+                        for(String line:lines){
+                            arrayLine.add(Double.parseDouble(line));
+                        }
                         valueList.add(arrayLine);
                     }
                 }
@@ -55,28 +58,33 @@ public class LineData {
         return variableNames.get(y);
     }
 
-    public ArrayList<Double> getValuesofVariable(String key) {
-        return dataset.get(key);
+    public double[] getValuesofVariable(String key) {
+        List<Double> arrayList = dataset.get(key);
+
+        double [] valueList = new double [arrayList.size()];
+        for (int i = 0; i < valueList.length; i++){
+            //System.out.println(arrayList.get(i));
+            valueList[i]= arrayList.get(i);    // hier kann es iwie nicht umwandeln
+            //System.out.println(valueList[i]);
+        }
+
+        return valueList;
         }
 
 
 
     /*public static void main(String[] args) {
-        LineData data = new LineData("/home/stefan/IdeaProjects/prog-II-project/src/main/resources/gauss-10000.lin");
+        LineData data = new LineData("/home/stefan/IdeaProjects/prog-II-project/src/main/resources/reference-data.lin");
         for (String word : data.variableNames) {
             System.out.println(word);
             //System.out.println();
         }
-        for (List element : data.valueList) {
-            System.out.println(element);
-        }
+
         for (Map.Entry<String, List<Double>> entry : data.dataset.entrySet()) {
             System.out.println(entry.getKey() + "->" + entry.getValue());
         }
+        data.getValuesofVariable(data.getNameOfVariables(0));
 
-
-
-        System.out.println(data.delimiter);
     }*/
 }
 
