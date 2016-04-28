43e3a5d18077718899247d52fd5f93ff0d9a0c08
stefankarlin <stefan.karlin@students.fhnw.ch>
2016-04-28 19:31:02 +0200

    Creating Class to read Line Datafiles

--- src/main/java/ch/fhnw/project/LineData.java
+++ src/main/java/ch/fhnw/project/LineData.java
@@ -11,8 +11,9 @@ public class LineData {
     // Fields:
     List<String> datalines;
     List<String> variableNames = new ArrayList<>();
-    List<Double> valueList = new ArrayList<>();
+    List<String> valueList = new ArrayList<>();
     Map<String,List<Double>> dataset = new HashMap<>();
+    String delimiter = "";
 
     //Constructor:
 
@@ -20,12 +21,21 @@ public class LineData {
         try {
             File file = new File(filePath);
             Scanner fileScanner = new Scanner(file);
+
             datalines = new ArrayList<>();
             int amountofVariables = Integer.parseInt(fileScanner.nextLine());
             int maxAmountofLines = 2*(amountofVariables+1);
             for (int row = amountofVariables; row<=maxAmountofLines; row++ ){
                 if(row <= (amountofVariables+1)){
-                variableNames.add(fileScanner.nextLine());}
+                variableNames.add(fileScanner.nextLine());
+                }
+                else if( row == amountofVariables+2){
+                    delimiter = fileScanner.nextLine();
+                    fileScanner.useDelimiter(delimiter);
+                }
+                else{
+                    valueList.add(fileScanner.nextLine());
+                }
 
             }
         }
@@ -40,30 +50,9 @@ public class LineData {
             System.out.println(word);
             //System.out.println();
         }
+        for (String element: data.valueList){
+            System.out.println(element);
+        }
+        System.out.println(data.delimiter);
     }
 }
-
-       /* //System.out.println("Amount: " + amountofVariables);
-        // fileScanner.useDelimiter(";");
-        while (fileScanner.hasNextLine() && !(fileScanner.nextLine().equals(";")))  {
-            String row = fileScanner.nextLine();
-            variableNames.add(row);
-        }
-    }
-    catch(Exception e){
-        System.out.println("File not found!");
-    }
-}
-
-    public List<Double> variableNames(){
-        for (String element:datalines){
-            try{
-                double value = Double.parseDouble(element);
-                valueList.add(value);
-
-            }catch(Exception e){
-                System.out.println("No Double");;
-            }
-        }
-        return valueList;
-    }*/
