aaca45df232be5fa32495c169d29d4714741f164
saschadietrich <sascha.dietrich@students.fhnw.ch>
2016-05-04 18:06:31 +0200

    txt files class ok! aber noch ohne interface

--- src/main/java/ch/fhnw/project/TxtReader.java
+++ src/main/java/ch/fhnw/project/TxtReader.java
@@ -3,48 +3,27 @@ package ch.fhnw.project;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
-public class TxtReader implements DataReader  {
+public class TxtReader  {
 
-    @Override
-    public DataModel creatingModel(File file) throws FileNotFoundException {
+    //@Override
+    public static DataModel creatingModel(File file) throws FileNotFoundException { // sobald inferface funktioniert wieder ranmachen
 
-        List<String> names = createVariableNameList(file);
-
-        List<double[]> valueList = new ArrayList<>();
         Scanner scanner = new Scanner(file);
-        int length = (Arrays.asList(scanner.nextLine().split("\\t"))).size();
+        String dateiname = "name muss noch hin";
+        DataModel datastructure = new DataModel( Arrays.asList(scanner.nextLine().split("\\t")),dateiname);
+        int numberOfVariable = (datastructure.getVariable()).size();
+        List<Variable> variable = datastructure.getVariable();
 
-        for(int i=0; i<length;i++){
-            valueList.add(createValuesList(file,i));
+        while (scanner.hasNextLine()){
+            String[] linearray = (scanner.nextLine().split("\\t"));
+            for(int i=0; i<numberOfVariable; i++){
+                (variable.get(i)).addValue(Double.parseDouble(linearray[i]));
+            }
         }
-        return null;
-
-    }
-
-
-    public List<String> createVariableNameList(File file) throws FileNotFoundException {
-
-        Scanner scanner = new Scanner(file);
-        return Arrays.asList(scanner.nextLine().split("\\t"));
-
-    }
-
-
-    public double[] createValuesList(File file, int index) throws FileNotFoundException {
-        List<Double> list = new ArrayList<>();
-        Scanner scanner = new Scanner(file);
-        int length = (Arrays.asList(scanner.nextLine().split("\\t"))).size(); // überspirngt erste zeile
-        while (scanner.hasNext()){
-            double string = Double.parseDouble((scanner.nextLine().split("\\t"))[index]);
-            list.add(string);
-        }
-        double[] values = new double[list.size()];
-
-        return null;
+        return datastructure;
     }
 }
