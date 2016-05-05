a7a3c0a6a8f3397de1912554e6a562049d5ff693
stefankarlin <stefan.karlin@students.fhnw.ch>
2016-05-05 10:45:04 +0200

    Korrektur LineReader-Klasse

--- src/main/java/ch/fhnw/project/LineReader.java
+++ src/main/java/ch/fhnw/project/LineReader.java
@@ -17,31 +17,26 @@ public class LineReader { //implements DataReader {
     public static DataModel creatingModel(File file) throws FileNotFoundException {
 
         Scanner scanner = new Scanner(file);
-        String filename = "";
+
         String delimiter = "";
-
-        List<String> filePath = new ArrayList(Arrays.asList(file.toString().split("/"))); //Weisst du warum das Orange ist?
-        for (String s : filePath) {
-            filename = filePath.get(filePath.lastIndexOf(s));
-        }
-
+        String filename = file.getName();
 
         int amountofVariables = Integer.parseInt(scanner.nextLine());
-        int maxAmountofLines = 2 * (amountofVariables + 1);
 
         List<String> variableNames = new ArrayList<>();
 
-        for (int row = amountofVariables; row <= maxAmountofLines; row++) { // Scannt durch alle Linien, wahrscheinlich nicht unbedingt nötig!
+        for (int row = amountofVariables; row <= (amountofVariables+2); row++) { //Scannt bis zum Delimiter
             if (row <= (amountofVariables + 1)) {
                 variableNames.add(scanner.nextLine());
-            } else if (row == amountofVariables + 2) {
+            }
+            else if (row == amountofVariables + 2) {
                 delimiter = scanner.nextLine();
             }
         }
 
         DataModel dataModel = new DataModel(variableNames, filename);
 
-        //Hinzufügen der Werte zu den Variablen-Objekten
+        //Hinzufügen der Werte zu den Variablen-Objekten, Scannt alle Linien nach dem Delimiter
         while (scanner.hasNextLine()) {
             for (int i = 0; i < (dataModel.getVariable().size()); i++) {    //dataModel.getVariable().size() --> ruft erst die liste der Variabeln und ab und bestimmt dann die Grösse!
                 List<String> lines = new ArrayList((Arrays.asList(scanner.nextLine().split(delimiter)))); //Weisst du warum das Orange ist? // Liest eine Linie un Speichert sie als String -Liste
@@ -50,7 +45,6 @@ public class LineReader { //implements DataReader {
                 }
             }
         }
-
         return dataModel;
     }
 
