ed013335a6fc42f8c8b8616cca997ec65c0eff5a
stefankarlin <stefan.karlin@students.fhnw.ch>
2016-05-05 09:47:02 +0200

    Creating Class to read Line Datafiles -- Es laeuft :)

--- src/main/java/ch/fhnw/project/DataModel.java
+++ src/main/java/ch/fhnw/project/DataModel.java
@@ -7,21 +7,21 @@ import java.util.stream.Collectors;
 
 public class DataModel {
     private List<Variable> variable;
-    private String dateiname;
+    private String filename;
 
-    public DataModel(List<String> names,String dateiname){
+    public DataModel(List<String> names,String filename){
         variable = names.stream().map(Variable::new).collect(Collectors.toList());
         /*List<Variable> variable = new ArrayList<>();
         for (String name : names) {
             variable.add(new Variable(name));*/
-        this.dateiname = dateiname;
+        this.filename = filename;
     }
 
     public List<Variable> getVariable(){
         return variable;
     }
 
-    public String getDateiname(){
-        return dateiname;
+    public String getFilename(){
+        return filename;
     }
 }
