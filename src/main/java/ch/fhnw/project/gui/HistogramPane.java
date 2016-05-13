61e85409994a23cb16c6ed0daca28d401e3d7c74
saschadietrich <sascha.dietrich@students.fhnw.ch>
2016-05-13 17:08:56 +0200

    Histogram

--- src/main/java/ch/fhnw/project/gui/HistogramPane.java
+++ src/main/java/ch/fhnw/project/gui/HistogramPane.java
@@ -5,7 +5,11 @@ import ch.fhnw.project.model.Variable;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.layout.Pane;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class HistogramPane extends Pane{
@@ -13,40 +17,89 @@ public class HistogramPane extends Pane{
     private CategoryAxis xAxis = new CategoryAxis();
     private NumberAxis yAxis = new NumberAxis();
     private BarChart<String,Number> barChart = new BarChart<>(xAxis, yAxis);
-
-    private static int numberOfPins;
-    private double min, max;
+    private static int numberOfBin;
 
     public HistogramPane(){
         barChart.setBarGap(0);
         barChart.setCategoryGap(0);
+        barChart.setLegendVisible(false);
+
         this.getChildren().add(barChart);
-
-
     }
-
 
     public void change(Variable variable){
         barChart.setTitle(variable.toString());
-        //System.out.println(numberOfPins);
-        //System.out.println(variable.getMax());
-        //System.out.println(variable.getMin());
-
+        
+        plot(biuldGroup(variable), getBinNames(variable));
 
     }
 
-    private void biuldGroup(Variable variable){
+    private int[] biuldGroup(Variable variable){
 
+        double step = getStepSize(variable);
+        int[] pins = new int[numberOfBin];
+
+        for(int i = 0; i< numberOfBin; i++){
+            pins[i]=0;
+        }
+        for (Double aDouble : variable.getValues()) {
+            for (int i = 1; i<= numberOfBin; i++){
+                if (aDouble<=(variable.getMin()+step*i)){
+                    pins[i-1]++;
+                    break;
+                }
+            }
+        }
+
+//        System.out.println("Pins: " +numberOfBins);
+//        System.out.println("Step: "+ step);
+//        System.out.println("Pins: " +numberOfBins);
+//        System.out.println("Step: "+ step);
+//        int total=0;
+//        for (int pin : pins) {
+//            total += pin;
+//            System.out.println(pin);
+//        }
+//        System.out.println("Size: "+ variable.getValues().size() + " Sum of Pinssize: "+ total);
+
+        return pins;
     }
 
-    private void stepsize(){
-
+    private double getStepSize(Variable variable) {
+        return (variable.getMax()-variable.getMin())/ numberOfBin;
     }
 
-    private void setMinMax(Variable variable){
+    private void plot(int[] pins,List<String> pinName){
 
+
+        barChart.getData().clear();
+        XYChart.Series<String, Number> dataPoints = new XYChart.Series<>();
+
+        for (int i = 0; i< numberOfBin; i++){
+            dataPoints.getData().add(new XYChart.Data<>(pinName.get(i),pins[i]));
+        }
+        barChart.getData().add(dataPoints);
     }
-    public static void setPin(DataModel dataModel){
-        numberOfPins = (int)Math.round(Math.sqrt(dataModel.getVariable().get(0).getValues().size()));
+
+    private List<String> getBinNames(Variable variable){
+
+        double step = getStepSize(variable);
+        List<String> names = new ArrayList<>();
+
+        for(int i = 1; i<= numberOfBin; i++){
+
+            names.add(format((variable.getMin()+step*i)/2));
+        }
+        return names;
+    }
+
+    private String format(double input) {
+        DecimalFormat decimalFormat = new DecimalFormat("#0.00");
+        double toFormat = ((double)Math.round(input*100))/100;
+        return decimalFormat.format(toFormat);
+    }
+
+    public static void setBin(DataModel dataModel){
+        numberOfBin = (int)Math.round(Math.sqrt(dataModel.getVariable().get(0).getValues().size()));
     }
 }
--- src/main/java/ch/fhnw/project/gui/MainPane.java
+++ src/main/java/ch/fhnw/project/gui/MainPane.java
@@ -11,7 +11,7 @@ public class MainPane extends Pane {
     public MainPane(DataModel dataModel){
 
         ScatterPlotPane scatterPlotPane = new ScatterPlotPane();
-        HistogramPane.setPin(dataModel); // da die Pin bei allen Variablen der gleichen Datei gleich ist --> static --> nur einmal machen
+        HistogramPane.setBin(dataModel); // da die Pin bei allen Variablen der gleichen Datei gleich ist --> static --> nur einmal machen
         HistogramPane histogramPane1 = new HistogramPane();
         HistogramPane histogramPane2 = new HistogramPane();
         ControlPane controlPane = new ControlPane(dataModel, scatterPlotPane, histogramPane1, histogramPane2);
