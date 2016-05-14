ccabd0311647b98c9c116a5f0527069b25eab77c
stefankarlin <stefan.karlin@students.fhnw.ch>
2016-05-14 20:54:39 +0200

    ControlPane:  Funktionen zum Aufruf des ScatterPlots angepasst (2 Variabeln für x und y hinzugefügt)  setComboBoxToDefault Funktion erstellt
     ScatterPlotPane:
     Daten können eingelesen werden und werden als Circles dargestellt
     -->noch zu erledigen (to Do Stefan):
     ScatterControlPanel in eigene Klasse Schreiben,
     Circle Grösse an Slider binden,
     Farbeinstellung der Circles,
     Vbox grösse anpassen,
     Ausprobieren mit Linechart

--- src/main/java/ch/fhnw/project/gui/ControlPane.java
+++ src/main/java/ch/fhnw/project/gui/ControlPane.java
@@ -6,10 +6,17 @@ import ch.fhnw.project.model.Variable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.layout.HBox;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ControlPane extends HBox {
     private DataModel dataModel;
     private ScatterPlotPane scatterPlotPane;
     private HistogramPane histogramPane1, histogramPane2;
+    private Variable variableX;
+    private Variable variableY;
+    private ComboBox<Variable> xChooser = new ComboBox<>();
+    private ComboBox<Variable> yChooser = new ComboBox<>();
 
     public ControlPane(DataModel dataModel, ScatterPlotPane scatterPlotPane, HistogramPane histogramPane1, HistogramPane histogramPane2){
         this.dataModel = dataModel;
@@ -17,41 +24,47 @@ public class ControlPane extends HBox {
         this.histogramPane1 = histogramPane1;
         this.histogramPane2 = histogramPane2;
 
-        ComboBox<Variable> xChooser = new ComboBox<>();
-        ComboBox<Variable> yChooser = new ComboBox<>();
-
         for (Variable variable : dataModel.getVariable()) {
             xChooser.getItems().add(variable);
             yChooser.getItems().add(variable);
         }
 
-        xChooser.setValue(dataModel.getVariable().get(0));
-        yChooser.setValue(dataModel.getVariable().get(1));
-        setNewValue(xChooser.getValue(),true);// muss noch mit start funktion ersetzt werden
-        setNewValue(yChooser.getValue(),false);
+        setComboBoxtoDefault(dataModel.getVariable().get(0), dataModel.getVariable().get(1));
 
         xChooser.valueProperty().addListener((observable, oldValue, newValue) -> {
-            setNewValue(newValue,true);
+            setNewValueHistogram(newValue,true);
+            variableX = newValue;
+            setNewValueScatterPlot(variableX,variableY);
         });
 
         yChooser.valueProperty().addListener((observable, oldValue, newValue) -> {
-            setNewValue(newValue,false);
+            setNewValueHistogram(newValue,false);
+            variableY = newValue;
+            setNewValueScatterPlot(variableX,variableY);
         });
 
         this.getChildren().addAll(xChooser,yChooser);
     }
 
-    private void setNewValue(Variable newVariable, boolean xValue){
-
-        if (xValue){
-            scatterPlotPane.setXVariable(newVariable);
-            histogramPane1.change(newVariable);
-        }else{
-            scatterPlotPane.setYVariable(newVariable);
-            histogramPane2.change(newVariable);
-        }
-
-
+    private void setComboBoxtoDefault(Variable x, Variable y){
+        xChooser.setValue(x);
+        yChooser.setValue(y);
+        variableX = x;
+        variableY = y;
+        setNewValueHistogram(xChooser.getValue(),true);
+        setNewValueHistogram(yChooser.getValue(), false);
+        setNewValueScatterPlot(x,y);
     }
 
+    private void setNewValueScatterPlot(Variable varX, Variable varY){
+        scatterPlotPane.createDataSeries(varX,varY);
+    }
+
+    private void setNewValueHistogram(Variable newVariable, boolean xValue){
+        if (xValue){
+            histogramPane1.change(newVariable);
+        }else{
+            histogramPane2.change(newVariable);
+        }
+    }
 }
--- src/main/java/ch/fhnw/project/gui/ScatterPlotPane.java
+++ src/main/java/ch/fhnw/project/gui/ScatterPlotPane.java
@@ -2,19 +2,87 @@ package ch.fhnw.project.gui;
 
 
 import ch.fhnw.project.model.Variable;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.ScatterChart;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.Slider;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 
-public class ScatterPlotPane extends Pane {
+import java.util.List;
+
+public class ScatterPlotPane extends StackPane {
+
+    private NumberAxis xAxis = new NumberAxis();
+    private NumberAxis yAxis = new NumberAxis();
+    private ScatterChart<Number,Number> scatterChart = new ScatterChart<>(xAxis,yAxis);
+    private XYChart.Series <Number,Number> dataSeries = new XYChart.Series<>();
+
+    //Eigene Klasse für das ControlPanel für den Scatter Plot erstellen!
+    VBox vbox = new VBox();
+    HBox scatterBox = new HBox();
+    HBox scatterPanelBox = new HBox();
+
+    Slider slider = new Slider();
+    ToggleGroup toggleGroup = new ToggleGroup();
+    RadioButton scatterRB = new RadioButton("Scatter Plot");
+    RadioButton lineRB = new RadioButton("Line Plot");
+    RadioButton bubbleRB = new RadioButton("Bubble Plot");
 
 
+   public void createDataSeries(Variable variableX, Variable variableY) {
 
-    public void setXVariable(Variable variable) {
-        for (Double aDouble : variable.getValues()) {
-            //System.out.println(aDouble);
-        }
+       //Formation of Axis:
+       xAxis.setForceZeroInRange(false);
+       yAxis.setForceZeroInRange(false);
+       xAxis.setLabel(variableX.toString());
+       yAxis.setLabel(variableY.toString());
+
+       //Resets previous Dataset, otherwise nasty erros
+       dataSeries.getData().clear();
+       scatterChart.getData().clear();
+
+       //Value Lists:
+       List<Double> xValues = variableX.getValues();
+       List<Double> yValues = variableY.getValues();
+
+       //Creates Data points, with sizable Circles
+       for (int i = 0; i < xValues.size(); i++) {   // Problem --> Was ist wenn X und Y nicht gleich gross sind!
+           XYChart.Data<Number,Number> dataPoint = new XYChart.Data<>(xValues.get(i),yValues.get(i));
+           Circle circle = new Circle();
+           circle.setRadius(5);
+           circle.setFill(Color.BLACK);
+           dataPoint.setNode(circle);
+           dataSeries.getData().add(dataPoint);
+       }
+       createPlot(dataSeries);
+   }
+
+    private void createPlot(XYChart.Series<Number,Number> data){
+
+        scatterChart.getData().add(data);
+
+        this.getChildren().clear();
+
+        //Eigene Klasse für ScatterPanel erstellen!
+        scatterRB.setToggleGroup(toggleGroup);
+        lineRB.setToggleGroup(toggleGroup);
+        bubbleRB.setToggleGroup(toggleGroup);
+        scatterRB.setSelected(true);
+
+        scatterPanelBox.getChildren().addAll(slider,scatterRB,lineRB,bubbleRB);
+        scatterBox.getChildren().add(scatterChart);
+        vbox.getChildren().addAll(scatterPanelBox,scatterBox);  //Vbox ist zu Klein noch anpassen!!
+        this.getChildren().addAll(vbox);
+
+
     }
 
-    public void setYVariable(Variable variable){
-        //System.out.println("olee");
-    }
+
 }
